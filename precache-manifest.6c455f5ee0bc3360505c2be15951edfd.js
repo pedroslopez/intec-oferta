024483bc24112fd5638f2d714ad8d4318261301a
Pedro Lopez <pedroslopez@me.com>
2019-04-28 19:09:30 -0400

    Updates

---
+++ precache-manifest.6c455f5ee0bc3360505c2be15951edfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9008855c73e5db1f20ab665b6fc0f0",
+    "url": "/intec-oferta/index.html"
+  },
+  {
+    "revision": "44c3ace4da6dd59917ef",
+    "url": "/intec-oferta/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "44c3ace4da6dd59917ef",
+    "url": "/intec-oferta/static/js/2.c890ccce.chunk.js"
+  },
+  {
+    "revision": "6d868b56deecb6fc4057",
+    "url": "/intec-oferta/static/js/main.72b32597.chunk.js"
+  },
+  {
+    "revision": "d20e6dd6403908fb55f7",
+    "url": "/intec-oferta/static/js/runtime~main.9ecb757e.js"
+  }
+]);
